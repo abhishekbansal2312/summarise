5e677f385f4c097d808494629327b432607cf8d3
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-03-15 02:17:40 +0530

    `add new sections`

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import HeroSection from "@/components/home/hero-section";
+import BgGradient from "@/components/common/bg-gradient";
+import DemoSection from "@/components/home/demo-section";
+import HowItWorksSection from "@/components/home/how-it-work";
+import PricingSection from "@/components/home/pricing-section";
+import CTASection from "@/components/home/cta-section";
 
 export default function page() {
   return (
     <div className="relative w-full">
-      <HeroSection />
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+        <DemoSection />
+        <HowItWorksSection />
+        <PricingSection />
+        <CTASection />
+      </div>
     </div>
   );
 }
