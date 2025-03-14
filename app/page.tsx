d6c69d1a204fd9d9b9e015c9a7731ad7f7b6a539
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-03-14 15:41:01 +0530

    Refactor font imports and update metadata in layout.tsx. initialise the shadcn.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-
+import { Button } from "@/components/ui/button";
 export default function page() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Summarise</h1>
+      <p>Summarise is an app to summarise text.</p>
+      <Button>Click me</Button>
+    </div>
+  );
 }
