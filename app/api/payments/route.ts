239d9f3cf8f88d44c6d8bfed8b610b53d9f93603
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-03-16 21:17:10 +0530

    Add Stripe webhook endpoint for handling checkout session completed events

---
+++ app/api/payments/route.ts
@@ -0,0 +1,50 @@
+import { handleCheckoutSessionCompleted } from "@/lib/payments";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+if (!stripeSecretKey) {
+  throw new Error("Stripe secret key is not defined");
+}
+const stripe = new Stripe(stripeSecretKey);
+
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const payload = await req.text();
+    const sig = req.headers.get("stripe-signature");
+
+    if (!sig || !endpointSecret) {
+      throw new Error("Missing Stripe signature or endpoint secret");
+    }
+
+    const event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+
+    switch (event.type) {
+      case "checkout.session.completed":
+        const sessionId = event.data.object.id;
+        const session = await stripe.checkout.sessions.retrieve(sessionId, {
+          expand: ["line_items"],
+        });
+
+        await handleCheckoutSessionCompleted({ session, stripe });
+
+        break;
+      case "customer.subscription.deleted":
+        const subscription = event.data.object.id;
+        console.log("Customer Subscription Deleted:", subscription);
+        break;
+      default:
+        console.log(`Unhandled event type: ${event.type}`);
+    }
+
+    return NextResponse.json({
+      status: "success",
+      message: "Webhook received",
+    });
+  } catch (err) {
+    console.error("Error processing webhook:", err);
+    return NextResponse.json({ error: "Invalid signature" }, { status: 400 });
+  }
+};
