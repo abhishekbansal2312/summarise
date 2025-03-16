4f55cd494e9c4a00c5b44aef9479634f5f96191f
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-03-17 03:19:53 +0530

    Add motion-wrapper component for animated elements

---
+++ components/common/motion-wrapper.tsx
@@ -0,0 +1,17 @@
+"use-client";
+import { motion } from "framer-motion";
+
+const MotionComponents = {
+  MotionDiv: motion.div,
+  MotionSection: motion.section,
+  MotionH1: motion.h1,
+  MotionH2: motion.h2,
+  MotionH3: motion.h3,
+  MotionH4: motion.h4,
+  MotionH5: motion.h5,
+  MotionH6: motion.h6,
+  MotionP: motion.p,
+  MotionSpan: motion.span,
+};
+
+export default MotionComponents;
