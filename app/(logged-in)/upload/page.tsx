ea999855dc389a32a07a3e2c9070050db578ff56
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-03-17 20:37:02 +0530

    Refactor upload page to add summary viewer and improve code structure

--- app/(logged-in)/upload/page.tsx
+++ app/(logged-in)/upload/page.tsx
@@ -1,10 +1,12 @@
 import BgGradient from "@/components/common/bg-gradient";
+import { SummaryViewerSkeleton } from "@/components/upload/loading-skeleton";
 import UploadForm from "@/components/upload/upload-form";
 import UploadHeader from "@/components/upload/upload-header";
 import { hasReachedUploadLimit } from "@/lib/user";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-
+import { SummaryViewer } from "@/components/summaries/summary-viewer";
+import { aboutUpload } from "@/utils/about";
 export default async function Page() {
   const user = await currentUser();
   const userId = user?.id;
@@ -30,11 +32,14 @@ export default async function Page() {
   }
 
   return (
-    <div className="relative min-h-screen">
+    <div className="relative min-h-screen flex flex-col items-center">
       <BgGradient />
-      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
+      <div className="w-full max-w-7xl px-6 py-8 sm:py-20 lg:px-8">
         <UploadHeader />
         <UploadForm />
+      </div>
+      <div className="w-full flex justify-center py-6 sm:py-8">
+        <SummaryViewer summary={aboutUpload} />
       </div>
     </div>
   );
